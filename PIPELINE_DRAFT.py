30a8e757a75795221b4126b9dec8a2d5a7fab884
ktsuo <60711835+ktsuo@users.noreply.github.com>
2021-08-17 15:56:40 -0400

    Add first draft

---
+++ PIPELINE_DRAFT.py
@@ -0,0 +1,58 @@
+import hail as hl
+import argparse
+import hailtop.batch as hb
+import pandas as pd
+
+def format_input(filepath, SNP, A1, A2, BETA, P):
+    if hl.hadoop_exists(filepath):
+        cols = [SNP, A1, A2, BETA, P]
+        summstats = pd.read_table(filepath, header=0, sep='\t', compression='gzip', usecols=cols)
+        summstats = summstats.rename(columns={SNP:'SNP', A1:'A1', A2:'A2', BETA:'BETA', P:'P'})
+        return summstats # how to make formatted summstats temporary j.ofile? 
+    else:
+        print('summstats file not found!')
+
+def main(args):
+    backend = hb.ServiceBackend(billing_project='', bucket='')
+
+    sst_list = []
+    pop_list = []
+    n_list = []
+    with open(args.input_file, 'rt') as f:
+        rows = f.readlines()
+    
+        for row in rows:
+            sst_path, pop, n = row.split('\t')
+            sst_list.append(sst_path)
+            pop_list.append(pop)
+            n_list.append(n)
+
+    b = hb.Batch(backend=backend, name='format-files-for-prscsx')
+    #jobs = []
+    format_sst_output = [] 
+    for sst in sst_list:
+        j = b.new_python_job(name=f'formatting-{sst}')
+        j.call(format_input, sst, args.SNP_col, args.A1_col, args.A2_col, args.A1_BETA_col, args.P_col) 
+        #jobs.append(j)
+        format_sst_output.append(j.ofile)
+
+
+    sink = b.new_job(name=f'run-prscsx')
+    sink.call(run_prscsx) # run prscsx function here?
+
+
+    
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_file', required=True)
+    parser.add_argument('--SNP_col', required=True)
+    parser.add_argument('--A1_col', required=True)
+    parser.add_argument('--A2_col', required=True)
+    parser.add_argument('--A1_BETA_col', required=True)
+    parser.add_argument('--P_col', required=True)
+    args = parser.parse_args()
+
+    main(args)
+
+
