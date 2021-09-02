dbc88d1560bb80dd66d969630b494f35c1a3c065
ktsuo <60711835+ktsuo@users.noreply.github.com>
2021-09-02 16:37:14 -0400

    Updated billing project

--- PIPELINE_DRAFT.py
+++ PIPELINE_DRAFT.py
@@ -54,7 +54,7 @@ def run_prscsx(b: hb.batch.Batch,
     return j
 
 def main(args):
-    backend = hb.ServiceBackend(billing_project='diverse-pop-seq-ref', bucket='gs://ukb-diverse-pops/prs-csx')
+    backend = hb.ServiceBackend(billing_project='ukb_diverse_pops', bucket='gs://ukb-diverse-pops/prs-csx')
 
     sst_list = []
     pop_list = []
