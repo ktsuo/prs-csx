a2bd45ebb8e159e5ac859937dabd4e7558fb0ba2
LindoNkambule <lindonkambule116@gmail.com>
2021-09-14 17:43:02 +0200

    minor changes

--- PIPELINE_DRAFT.py
+++ PIPELINE_DRAFT.py
@@ -2,93 +2,120 @@ import hail as hl
 import argparse
 import hailtop.batch as hb
 import pandas as pd
+import os
+from typing import List
 
-def format_input(filepath, SNP, A1, A2, BETA, P):
-    if hl.hadoop_exists(filepath):
-        
-        basename = os.path.basename(filepath)
-        root, extension = os.path.splitext(basename)
-        
-        cols = [SNP, A1, A2, BETA, P]
-        summstats = pd.read_table(filepath, header=0, sep='\t', compression='gzip', usecols=cols)
-        summstats = summstats.rename(columns={SNP:'SNP', A1:'A1', A2:'A2', BETA:'BETA', P:'P'})
-                
-        outfile_name = f'{root}_formatted.txt'
-        summstats.to_csv(outfile_name, sep='\t')
 
-        return outfile_name
-    else:
-        print('summstats file not found!')
+def format_input(filepath, SNP, A1, A2, BETA, P, outdir):
+    print(f'formating {filepath}')
+    basename = os.path.basename(filepath)
+    root, extension = os.path.splitext(basename)
+
+    cols = [SNP, A1, A2, BETA, P]
+    summstats = pd.read_table(filepath, header=0, sep='\t', compression='gzip', usecols=cols)
+    summstats = summstats.rename(columns={SNP: 'SNP', A1: 'A1', A2: 'A2', BETA: 'BETA', P: 'P'})
+
+    outfile_name = f'{root}_formatted.txt'
+    summstats.to_csv(f'{outdir}/formated_sst_files/{outfile_name}', sep='\t', index=False)
+
+
+def run_prscsx(b: hb.batch.Batch,
+               image: str,
+               bim_file: str,
+               summary_stats: List,
+               N: List,
+               pops: List,
+               chr: int,
+               meta):
+
+    j = b.new_job(name=f'run-prscsx-{chr}')
+
+    j.image(image)
+    j.cpu(4)
+
+    # get bfile
+    input_bfile = b.read_input(bim_file)
+    bim_basename = os.path.basename(bim_file)
+    bim_root, _ = os.path.splitext(bim_basename)
+
+    sst_files = ''
+
+    for file in summary_stats:
+        input_sst = b.read_input(file)
+        sst_files += f'{input_sst},'
+
+    # format populations for input
+    pop_list = ','.join(pops)
+
+    # format sample sizes for input
+    n_list = ','.join(map(str, N))
+
+    j.command(f'''
+    mkdir tmp_prscsx_output
+    python3.8 PRS-CSx.py \
+        --ref_dir=ref_panels \
+        --bim_prefix={bim_root} \
+        --sst_file={sst_files} \
+        --n_gwas={n_list} \
+        --pop={pop_list} \
+        --out_dir=tmp_prscsx_output \
+        --out_name=tmp \
+        --chrom={chr} \
+        --meta={meta}''')
+
 
 def main(args):
-    backend = hb.ServiceBackend(billing_project='', bucket='')
+    backend = hb.ServiceBackend(billing_project='ukb_diverse_pops', bucket='gs://ukb-diverse-pops/prs-csx')
 
     sst_list = []
     pop_list = []
     n_list = []
     with open(args.input_file, 'rt') as f:
         rows = f.readlines()
-    
-        for row in rows:   ### pheno col?
-            sst_path, pop, n = row.split('\t')
+
+        for row in rows:
+            sst_path, pop, n = row.strip().split('\t')
             sst_list.append(sst_path)
             pop_list.append(pop)
             n_list.append(n)
 
+    print(sst_list)
+    print(pop_list)
+    print(n_list)
+
     b = hb.Batch(backend=backend, name='prscsx')
-    #jobs = []
-    format_sst_output = [] 
+    image = 'gcr.io/ukbb-diversepops-neale/ktsuo-prscsx'
+
     for sst in sst_list:
-        j = b.new_python_job(name=f'formatting-{sst}')
-        res = j.call(format_input, sst, args.SNP_col, args.A1_col, args.A2_col, args.A1_BETA_col, args.P_col) 
-        #jobs.append(j)
-        format_sst_output.append(res)
+        format_input(filepath=sst, SNP=args.SNP_col, A1=args.A1_col, A2=args.A2_col, BETA=args.A1_BETA_col,
+                     P=args.P_col, outdir=args.out_dir)
 
- 
-## RUN PRS-Csx
-def run_prs(batch: hb.batch.Batch, 
-            image: str,
-            ref_dir: str, 
-            bim: hb.resource.ResourceFile, 
-            summary_stats: List,
-            N: int,
-            chr:int,
-            pops: List,
-            out_name: str,
-            out_dir: str)-> hb.job.Job:
-    
-    
-   
-    prs.image(image)
-    prs.cpu(4)
-            
-    prs.command = f '" python3 {ref_dir}PRS-CSx.py \
-        --ref_dir={ref_dir} \ #from input
-        --bim_prefix={bim} \ #finput
-        --sst_file={sst_list} \ #from above
-        --n_gwas={n_list} \ #from above
-        --pop={pop_list} \ #from above
-        --out_dir={out_dir} \ #input
-        --out_name={pheno} \ #from above
-        --chrom={chrom} \ 
-        --meta=TRUE \
-        " '
-    
-    prs.depends_on(*jobs)
-     
-    for chr in range (1,22):chrom = f'chrom{chr}
-    
-    prs = batch.new_job(name = f'{pheno}-{chrom}-PRS-Csx')
-    prs = run_prs(ref_dir, bim, out_dir)
-            
-    
-    
-    
-    return prs
+    # format summstats string names for input
+    sst_file_paths = hl.utils.hadoop_ls(f'{args.out_dir}/formated_sst_files')
+    sst_list = []
+    for i in sst_file_paths:
+        sst_list.append(i['path'])
+
+    # get ref panels and untar
+    refpanels = {}
+    anc_list = ['afr', 'amr', 'eas', 'eur', 'sas']
+    for anc in anc_list:
+        file = os.path.join(args.ref_path, f'ldblk_ukbb_{anc}.tar.gz')
+        refpanels[anc] = b.read_input(file)
+
+    get_refpanels = b.new_job(name='get-ref-panels')
+    get_refpanels.command('mkdir ref_panels')
+
+    for ancestry, input_file in refpanels.items():
+        get_refpanels.command(f'tar -zwvf {input_file} --directory ref_panels')
+
+    for chrom in range(1, 23):
+        run_prscsx(b, image, bim_file=args.bfile_path, summary_stats=sst_list, N=n_list, pops=pop_list, chr=chrom,
+                   meta=args.meta)
+
+    b.run()
 
 
-    
-    
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--input_file', required=True)
@@ -97,12 +124,12 @@ if __name__ == '__main__':
     parser.add_argument('--A2_col', required=True)
     parser.add_argument('--A1_BETA_col', required=True)
     parser.add_argument('--P_col', required=True)
-    parser.add_argument('--chr', default='1-22')
-    parser.add_argument('--ref_dir', required=True)
-    parser.add_argument('--bim', required = True)
-    parser.add_argument('--out_dir', required = True)                   
-    args = parser.parse_args()
+    parser.add_argument('--bfile_path', required=True)
+    parser.add_argument('--ref_path', required=True)
+    parser.add_argument('--out_dir', required=True)
+    parser.add_argument('--meta', action="store_true")
+    arguments = parser.parse_args()
 
-    main(args)
+    main(arguments)
 
-
+    # python PIPELINE_DRAFT.py --input_file tmp_prscsx_inputfile_trialAFR.txt --SNP_col rsid --A1_col ALT --A2_col REF --A1_BETA_col inv_var_meta_beta --P_col inv_var_meta_p --bfile_path 'gs://ukb-diverse-pops/ktsuo_unrelateds_tmp/AFR/' --ref_path 'gs://ukb-diverse-pops/prs-csx/'
